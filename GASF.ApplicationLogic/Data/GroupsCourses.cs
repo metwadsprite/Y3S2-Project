3a271eb3ebf8de4da7ac27d19445a305d8ad644e
AlexandraStoienescu <alexandra.laura.st@gmail.com>
2020-04-09 22:24:56 +0300

    Model and repositories for student

---
+++ GASF.ApplicationLogic/Data/GroupsCourses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GASF.ApplicationLogic.Data
+{
+    class GroupsCourses
+    {
+       
+        public Course Course { get; set; } 
+
+        public int CourseId { get; set; }
+
+        public Group Group { get; set; }
+
+        public Guid GroupId { get; set; }
+    }
+}
