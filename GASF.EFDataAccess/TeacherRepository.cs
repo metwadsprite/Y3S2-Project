bf6fffbb10b1852010b4269ad48fa11015f8e3e5
AndreeaMariaRadu <radua6494@gmail.com>
2020-04-10 00:06:22 +0300

    adding methods implementation in repo

---
+++ GASF.EFDataAccess/TeacherRepository.cs
@@ -0,0 +1,23 @@
+﻿using GASF.ApplicationLogic.Abstractions;
+using GASF.ApplicationLogic.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GASF.EFDataAccess
+{
+    class TeacherRepository : BaseRepository<Teacher>, ITeacherRepository
+    {
+
+        public TeacherRepository(StudentRecordDbContext dbContext) : base(dbContext)
+        {
+
+        }
+        Teacher GetTeacherByName(string LastName)
+        {
+            return dbContext.Teachers
+                            .Where(teacher => teacher.LastName == LastName)
+                            .SingleOrDefault();
+        }
+    }
+}
